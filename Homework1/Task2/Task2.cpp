83fa0c78365d430542a18a049bc5d89fd8fabf7b
SpinSpinSugar <sem9.zernov@yandex.ru>
2022-09-09 00:57:15 +0300

    update

--- Homework1/Task2/Task2.cpp
+++ Homework1/Task2/Task2.cpp
@@ -16,19 +16,20 @@ void A::print_z() { std::cout << z << '\n'; }
 */
 
 int main() {
-    A tmp;
+    A obj;
 
-    int* addr_of_x = reinterpret_cast<int*>(&tmp);
+    int* addr_of_x = reinterpret_cast<int*>(&obj);
     std::cout << "Initialized x = " << std::hex << *addr_of_x << '\n';
     *addr_of_x = 0xBEEF;
     std::cout << "Changed x = " << std::hex << *addr_of_x << '\n';
-    int* addr_of_y = reinterpret_cast<int*>(&tmp);
+    
+    int* addr_of_y = reinterpret_cast<int*>(&obj);
     ++addr_of_y;
     std::cout << "Initialized y = " << std::hex << *addr_of_y << '\n';
     *addr_of_y = 0xC;
     std::cout << "Changed y = " << std::hex << *addr_of_y << '\n';
 
-    double* addr_of_z = reinterpret_cast<double*>(&tmp);
+    double* addr_of_z = reinterpret_cast<double*>(&obj);
     ++addr_of_z;
     std::cout << "Initialized z = " << std::hexfloat << *addr_of_z << '\n';
     *addr_of_z = 0xAAAAp0;
