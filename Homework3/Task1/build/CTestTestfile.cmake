9ea9d11e5db8c055ee3357a53eed89efd5354c20
SpinSpinSugar <sem9.zernov@yandex.ru>
2022-10-03 20:27:33 +0300

    cleaning

--- Homework3/Task1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chansonsdamour/Desktop/Repositories/cpp_programming_semester3/Homework3/Task1
-# Build directory: /home/chansonsdamour/Desktop/Repositories/cpp_programming_semester3/Homework3/Task1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/chansonsdamour/Desktop/Repositories/cpp_programming_semester3/Homework3/Task1/build/Tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
