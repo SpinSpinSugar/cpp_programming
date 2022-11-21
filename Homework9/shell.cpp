cd58053173845ebb213ab01cc180c32d4ddb9036
SpinSpinSugar <sem9.zernov@yandex.ru>
2022-11-21 19:35:27 +0300

    Added big 5 to shell class

--- Homework9/shell.cpp
+++ Homework9/shell.cpp
@@ -1,27 +1,75 @@
 #include "shell.hpp"
 #include <iostream>
+#include <functional>
 
 int sum(int x, int y) {
     return x + y;
 }
 
-auto lam = [](){ return 5; };
+struct Callabl {
+    int operator()() { return 5;}
+};
+
+struct StructWithFunct {
+    int a = 999;
+    int f() { return a; }
+};
+
 
 
 int main() {
-    auto lam2 = [=](int* a, int* b) { return a - b; };
-    std::string filename = "log.txt";
-    //std::fstream output {filename, output.out};
-    shell<int(int,int)> f{sum, filename};
-    f(1,2);
-    f(2, -10);
-    //std::fstream output2 {"log2.txt", output2.out};
-    shell<int()> g{lam, filename};
-    g();
+    std::string filename = "CallHistory.log";
+    
+    shell<int(int,int)> first;
+    shell<int(int,int)> third;
+    auto lambd = [](){ return 100; };
 
-    shell<decltype(lam2(nullptr,nullptr))(int*,int*)> g2 {lam2, filename};
+    //copy and move assigment + dtor correctness
+    {
+        shell<int(int,int)> second{sum, filename};
+        second(2, -10);
+        second(152, 253);
+        first = second;
+        third = std::move(second);
+    }
+    
+    //movable
+    third(203, 12035);
+
+    //move ctor;
+    shell<int()> tmp = [](){ return 42;};
+    auto real {std::move(tmp)};
+
+    // call after move - UB
+    //tmp();
+
+    //copyable
+    shell<int(int,int)> real2{sum};
+    
+    auto newReal {real2};
+    newReal(12, 15);
+    
+    //call after copy
+    real2(12, 15);
+
+    
+    //lambda is working
+    shell<int()> g{lambd, filename};
+    g();
+    
+    auto lam2 = [](int* a, int* b){ return a - b; };
+    shell<decltype(lam2(nullptr, nullptr))(int*, int*)> g2 {lam2, filename};
     int a, b;
     g2(&a, &b);
+    
+    //functor
+    Callabl c;
+    shell<int()> c_wrapper {c, filename};
+    c_wrapper();
 
-    return 0;
+    // ptr to member function
+    StructWithFunct s;
+    shell<int()> s_wrapper = std::bind(&StructWithFunct::f, &s); // WOW!
+    s_wrapper.change_filename(filename);
+    std::cout << s_wrapper() << '\n';
 }
--- Homework9/shell.hpp
+++ Homework9/shell.hpp
@@ -1,6 +1,11 @@
 #include <memory>
 #include <functional>
 #include <fstream>
+#include <ctime>
+#include <chrono>
+#ifdef DEBUG
+    #include <iostream>
+#endif
 
 namespace details {
     static unsigned long long COUNTER = 0;
@@ -9,61 +14,131 @@ namespace details {
 template <typename T>
 class shell;
 
-template <typename Ret, typename... Params>
-class shell<Ret(Params...)> {
+template <typename Res, typename... Args>
+class shell<Res(Args...)> {
 
 public:
-    template <typename FunctionObject>
-    shell(FunctionObject fo, std::string filename)
-        : callable{std::make_unique<callable_impl<FunctionObject>>(std::move(fo))},
-        Filename(filename)
-    {}
+    template <typename FunctionObject> requires std::is_invocable_v<FunctionObject, Args...>
+    shell(FunctionObject fo, std::string fname = {})
+        : fun{new Derived<FunctionObject>(fo)},
+        filename{fname}
+    { 
+    #ifdef DEBUG
+        std::cout << "CTOR\n";
+    #endif
+    }
 
-    Ret operator()(Params... params) {
-        Ret res = callable->call(params...);
+    shell() = default;
+    
+    shell(const shell& other) : fun{other.fun->get_copy()}, filename{other.filename} {
+        //TODO
+    #ifdef DEBUG
+        std::cout << "COPY CTOR\n";
+    #endif
+    }
+    
+    shell(shell&& other) : fun{}, filename{} {
+    #ifdef DEBUG
+        std::cout << "MOVE CTOR\n";
+    #endif
+        std::swap(fun, other.fun);
+        std::swap(filename, other.filename);
+    }
+    
+    shell& operator=(const shell& rhs) {
+    #ifdef DEBUG
+        std::cout << "COPY =\n";
+    #endif
+        
+        //allocating memory
+        auto tmp = rhs.fun->get_copy();
+        
+        if (fun != nullptr) fun->destroy();
+        fun = tmp;
+        filename = rhs.filename;
+        return *this;
+    }
+
+    shell& operator=(shell&& rhs) {
+    #ifdef DEBUG
+        std::cout << "MOVE =\n";
+    #endif
+        std::swap(fun, rhs.fun);
+        std::swap(filename, rhs.filename);
+        return *this;
+    }
+
+    Res operator()(Args&&... args) {
+        if (fun == nullptr) { abort(); }
+        Res res = (*fun)(std::forward<Args>(args)...); 
         ++details::COUNTER;
-        std::fstream outfile;
-        outfile.open(Filename, outfile.out | std::ios::app);
+        std::fstream outfile{filename, outfile.out | std::ios::app};
+        auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        outfile << ctime(&t) << ' ';
         outfile << details::COUNTER << ". Function " << typeid(*this).name() << " called with arguments: (";
-        print_args(outfile, params...);
+        print_args(outfile, args...);
         outfile << ')' << " Result: " << res << '\n';
-        outfile.close();
-        outfile.clear();
         return res;
     }
 
-private:
-    
-    void print_args(std::fstream& outfile) {}
+    ~shell() {
+#ifdef DEBUG
+        std::cout << "DTOR\n";
+#endif
+        if (fun != nullptr) fun->destroy();
+        delete fun;
+    };
 
-    template <typename T> void print_args(std::fstream& outfile, const T& t) {
-        outfile << t;
+
+private:
+    struct Base {
+        virtual Res operator()(Args&&...) = 0;
+        virtual Base* get_copy() = 0;
+        virtual void destroy() = 0;
+        virtual ~Base() = default;
+    };
+
+    template <typename F>
+    struct Derived : Base {
+        F callable;
+        Derived(const F& callable_)
+            : callable{callable_}
+        {}
+        Derived(const Derived& other) : callable{other.callable} {}
+        Base* get_copy() override { 
+            return new Derived<F>{callable};
+        }
+
+        Res operator()(Args&&... args) override {
+            return std::invoke(callable, std::forward<Args>(args)...);
+        }
+        void destroy() override { 
+            callable.~F();
+#ifdef DEBUG
+            std::cout << "CALLABLE DTOR\n";
+#endif
+        }
+        ~Derived() {}
+    };
+
+private:
+    void print_args([[maybe_unused]] std::fstream& outfile) {}
+
+    template <typename T> void print_args(std::fstream& outfile, T&& t) {
+        outfile << std::forward<T>(t);
     }
     
     template <typename First, typename... Rest>
-    void print_args(std::fstream& outfile, const First& first, const Rest&... rest) {
-        outfile << first << ", ";
+    void print_args(std::fstream& outfile, First&& first, Rest&&... rest) {
+        outfile << std::forward<First>(first) << ", ";
         print_args(outfile, rest...); 
     }
 
+    Base* fun = nullptr;
+    mutable std::string filename;
 
-    struct callable_interface {
-        virtual Ret call(Params...) = 0; 
-        virtual ~callable_interface() = default;
-    };
-
-    template <typename Callable>
-    struct callable_impl : callable_interface {
-        callable_impl(Callable callable_)
-            : callable{std::move(callable_)}
-        {}
-        Callable callable;
-        Ret call(Params... params) {
-            return std::invoke(callable, params...);
-        }
-    };
-
-    std::unique_ptr<callable_interface> callable;
-    std::string Filename;
-
+public:
+    void change_filename(const std::string& newfilename) const {
+        filename = newfilename;
+    }
 };
